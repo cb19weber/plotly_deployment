3bfe3930082bdd5503459a62559ae661a9d37e42
Cullen Weber <webercullen@gmail.com>
2022-09-05 16:24:54 -0600

    through module 12.4.1

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,27 +1,4 @@
-// Establish connection to data
-const cityData = cityGrowths;
-
-var sortedCities = cityData.sort((growthA, growthB) => growthB.Increase_from_2016 - growthA.Increase_from_2016);
-
-var topFiveCities = sortedCities.slice(0,7);
-// console.log(topFiveCities);
-
-var topFiveCityNames = topFiveCities.map(cName => cName.City);
-var topFiveCityGrowths = topFiveCities.map(cPop => parseInt(cPop.Increase_from_2016));
-
-trace = [{
-    x: topFiveCityNames,
-    y: topFiveCityGrowths,
-    type: "bar"
-}];
-layout = {
-    title: "Fastest Growing Cities",
-    xaxis: {title: "City"},
-    yaxis: {title: "Population Growth, 2016-2017"}
-};
-
-Plotly.newPlot("plotArea", trace, layout);
-
+// Retreive data from json API file
 d3.json("static/js/samples.json").then(function(data){
     wfreq = data.metadata.map(person => person.wfreq).sort((a,b) => b - a);
     filteredWfreq = wfreq.filter(element => element != null);
